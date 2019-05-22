b82220011ba74be21e898daca13ef90693270ebd
LordNani <rigansu@gmail.com>
2019-05-22 23:31:10 +0300

    Succesfully implemented pencil

--- Paint 2.0/Source.cpp
+++ Paint 2.0/Source.cpp
@@ -2,63 +2,67 @@
 
 #include <vector>
 
-void drawOnImage(sf::Color color, int tool, sf::Texture canvas, sf::Image image, sf::Vector2<int> vector2i);
+	void drawOnImage(sf::Color color, int tool, sf::Texture canvas, sf::Image &image, sf::Vector2i vector);
 
-int main()
-{
-	int windowHeight,windowWidth; //Window size that is specified by user's input to GUI
-	windowHeight = 600;
-	windowWidth = 800;
-	// create the window
-	sf::RenderWindow mainWindow(sf::VideoMode(windowWidth, windowHeight), "MainWindow");
-	sf::Image image;
-	image.create(windowWidth, windowHeight, sf::Color::White);
-	sf::Texture canvas;
-	sf::Color currentColor = sf::Color::Black;  // Color that user's chosen
-	int currentTool = 0; // instrument that is currently chosen
-	sf::Sprite sprite;
-	int mousedown = 0;
-	mainWindow.setFramerateLimit(60);
-	canvas.loadFromImage(image);
-	sprite.setTexture(canvas);
-
-	while (mainWindow.isOpen())
+	int main()
 	{
-		sf::Event event;
-		while (mainWindow.pollEvent(event))
+		int windowHeight, windowWidth; //Window size that is specified by user's input to GUI
+		windowHeight = 600;
+		windowWidth = 800;
+		// create the window
+		sf::RenderWindow mainWindow(sf::VideoMode(windowWidth, windowHeight), "MainWindow");
+		sf::Image image;
+		image.create(windowWidth, windowHeight, sf::Color::White);
+		sf::Texture canvas;
+		sf::Color currentColor = sf::Color::Black;  // Color that user's chosen
+		int currentTool = 0; // instrument that is currently chosen
+		sf::Sprite sprite;
+		int mousedown = 0;
+		mainWindow.setFramerateLimit(30);
+		canvas.loadFromImage(image);
+		sprite.setTexture(canvas);
+
+		while (mainWindow.isOpen())
 		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed)
-				mainWindow.close();
-			else if ((event.type == sf::Event::MouseMoved) && (mousedown == 1)) // MouseMoved and leftClickHeld
+			canvas.loadFromImage(image);
+			sf::Event event;
+			while (mainWindow.pollEvent(event))
 			{
-				canvas.update(mainWindow);
-				drawOnImage(currentColor, currentTool, canvas, image, sf::Mouse::getPosition(mainWindow));
+				// "close requested" event: we close the window
+				if (event.type == sf::Event::Closed)
+					mainWindow.close();
+				else if ((event.type == sf::Event::MouseMoved) && (mousedown == 1)) // MouseMoved and leftClickHeld
+				{
+					
+					
+					drawOnImage(currentColor, currentTool, canvas, image, sf::Mouse::getPosition(mainWindow));
+				}
+				else if (event.type == sf::Event::MouseButtonPressed)
+				{
+					mousedown = 1;
+					drawOnImage(currentColor, currentTool, canvas, image, sf::Mouse::getPosition(mainWindow));
+					
+				}
+				else if (event.type == sf::Event::MouseButtonReleased)
+				{
+					mousedown = 0;
+					canvas.update(mainWindow);
+
+				}
 			}
-			else if (event.type == sf::Event::MouseButtonPressed)
-			{
-				mousedown = 1;
-				drawOnImage(currentColor, currentTool, canvas, image, sf::Mouse::getPosition(mainWindow));
-			}
-			else if (event.type == sf::Event::MouseButtonReleased)
-			{
-				mousedown = 0;
-				canvas.update(mainWindow);
-				
-			}
+			mainWindow.clear();
+			mainWindow.draw(sprite);
+			mainWindow.display();
+
+
 		}
-		mainWindow.clear();
-		mainWindow.draw(sprite);
-		mainWindow.display();
-		
-	 
+
+		return 0;
 	}
 
-	return 0;
-}
-
-void drawOnImage(sf::Color color, int tool, sf::Texture canvas, sf::Image image,sf::Vector2<int> vector2i) {
-	if (tool == 0) {
-		image.setPixel(vector2i.x, vector2i.y, color);
+	void drawOnImage(sf::Color color, int tool, sf::Texture canvas, sf::Image &image, sf::Vector2i vector) {
+		if (tool == 0) {
+			image.setPixel(vector.x, vector.y, color);
+			
+		}
 	}
-}
